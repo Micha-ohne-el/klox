b1fe4e821386c20fbf9a4376db5fdf49e3afdb5f
Micha-ohne-el <michalehmann0112@gmail.com>
2022-02-18 18:30:15 +0100

    Write II.5 — Representing Code

---
+++ src/main/kotlin/lox/AstPrinter.kt
@@ -0,0 +1,28 @@
+package lox
+
+class AstPrinter : Expression.Visitor<String> {
+    fun print(expression: Expression) = expression.accept(this)
+
+    override fun visit(literalExpression: Expression.Literal): String {
+        return (literalExpression.value ?: "nil").toString()
+    }
+
+    override fun visit(prefixExpression: Expression.Prefix): String {
+        return parenthesize(prefixExpression.operator.lexeme, prefixExpression.right)
+    }
+
+    override fun visit(binaryExpression: Expression.Binary): String {
+        with (binaryExpression) {
+            return parenthesize(operator.lexeme, left, right)
+        }
+    }
+
+    override fun visit(groupingExpression: Expression.Grouping): String {
+        return parenthesize("group", groupingExpression.expression)
+    }
+
+    private fun parenthesize(name: String, vararg expressions: Expression): String {
+        val parts = expressions.joinToString(" ") {expr -> expr.accept(this)}
+        return "($name $parts)"
+    }
+}
---
+++ src/main/kotlin/lox/Expression.kt
@@ -0,0 +1,39 @@
+package lox
+
+abstract class Expression {
+    open class Literal(
+        val value: Any?
+    ) : Expression() {
+        override fun <R> accept(visitor: Visitor<R>) = visitor.visit(this)
+    }
+
+    open class Prefix(
+        val operator: Token,
+        val right: Expression
+    ) : Expression() {
+        override fun <R> accept(visitor: Visitor<R>) = visitor.visit(this)
+    }
+
+    open class Binary(
+        val left: Expression,
+        val operator: Token,
+        val right: Expression
+    ) : Expression() {
+        override fun <R> accept(visitor: Visitor<R>) = visitor.visit(this)
+    }
+
+    open class Grouping(
+        val expression: Expression
+    ) : Expression() {
+        override fun <R> accept(visitor: Visitor<R>) = visitor.visit(this)
+    }
+
+    interface Visitor<R> {
+        fun visit(literalExpression: Literal): R
+        fun visit(prefixExpression: Prefix): R
+        fun visit(binaryExpression: Binary): R
+        fun visit(groupingExpression: Grouping): R
+    }
+
+    abstract fun <R> accept(visitor: Visitor<R>): R
+}
